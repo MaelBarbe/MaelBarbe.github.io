6a506762c9ca6638a41ba56841e7296ab374f1ce
Maël Barbé <mael.barbe47@gmail.com>
2025-06-21 20:08:40 +0200

    rename projects.js to last-projects.js

--- JS/last-projects.js
+++ JS/last-projects.js
@@ -1,17 +1,18 @@
 fetch('../data/projects.json')
     .then(response => response.json())
     .then(projects => {
-        const container = document.getElementById('projects-cards-container');
+        const container = document.getElementById('projects_container');
 
         const lastProjects = projects.slice(-3)
         lastProjects.forEach(projects => {
             const div = document.createElement('div');
-            div.classList.add('projet-card');
+            div.classList.add('project_card');
             div.innerHTML = `
             <a href="#">
-                <h3 class="projet-card_title">${projects.name}</h3>
-                <p class="projet-card_desc">${projects.desc}</p>
-                <p class="projet-card_technos">${projects.techno}</p>
+                <h3>${projects.name}</h3>
+                <img src="${projects.image}" alt="${projects.name}">
+                <p class="project_desc">${projects.desc}</p>
+                <p class="project_technos">${projects.techno}</p>
             </a>
             `;
             container.appendChild(div);
