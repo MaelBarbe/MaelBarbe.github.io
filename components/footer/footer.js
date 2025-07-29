92dda9f0aa74d51c7c6a080da9b0ce6038372c98
Maël Barbé <mael.barbe47@gmail.com>
2025-07-29 09:46:14 +0200

    Refactor footer structure and add CV page for improved navigation and content organization

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,1 +1,18 @@
-const navbarfooter = document.getElementById("navbar-footer")
+document.addEventListener('DOMContentLoaded', loadFooter);
+
+async function loadFooter() {
+    try {
+        const footer = document.getElementById('footer');
+        const reponse = await fetch('/components/footer/footer.html');
+
+        if (!reponse.ok) {
+            throw new Error('Erreur lors du chargement du footer');
+        }
+
+        const html = await reponse.text();
+        footer.innerHTML = html;
+
+    } catch (erreur) {
+        console.error('Erreur :', erreur);
+    }
+}
